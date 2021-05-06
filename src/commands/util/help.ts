a82a7e297af62e39e6e75c046a1b4a340e6e2e03
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-05-06 16:47:27 +0200

    :sparkles: add simple help command

---
+++ src/commands/util/help.ts
@@ -0,0 +1,45 @@
+import { Message, MessageEmbed } from "discord.js";
+import Command from "../../structures/Command";
+import Bot from "../../structures/Bot";
+
+export default class HelpCommand extends Command {
+  constructor(bot: Bot) {
+    super(bot, {
+      name: "help",
+      aliases: ["h"],
+      description: "Simple help command",
+      category: "util",
+    });
+  }
+
+  async execute(bot: Bot, message: Message, args: string[]) {
+    try {
+      const [commandName] = args;
+      const commands = bot.commands;
+
+      if (commandName) {
+        const command =
+          commands.get(commandName) ?? bot.commands.get(bot.aliases.get(commandName)!);
+
+        if (!command) {
+          return message.channel.send("Command not found!");
+        }
+
+        const embed = new MessageEmbed()
+          .setTitle(command.name)
+          .setDescription(command.options.description ?? "No description");
+
+        return message.channel.send(embed);
+      } else {
+        const embed = new MessageEmbed()
+          .setTitle(`${bot.user?.username}'s Commands`)
+          .setDescription(`\`\`\`${commands.map((cmd) => cmd.name).join(", ")}\`\`\``);
+
+        return message.channel.send(embed);
+      }
+    } catch (err) {
+      console.error(err);
+      return message.channel.send("An unexpected error occurred");
+    }
+  }
+}
